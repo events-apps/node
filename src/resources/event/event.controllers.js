8f7b37bd9268948593c9358c379dd836a631828c
Dawid Kobierski <dkobierski@objectivity.co.uk>
2020-02-20 10:15:13 +0100

    Implement events search and alter events model

--- src/resources/event/event.controllers.js
+++ src/resources/event/event.controllers.js
@@ -1,4 +1,24 @@
 import { crudControllers } from '../../utils/crud'
 import { Event } from './event.model'
 
-export default crudControllers(Event)
+export const getMany = async (req, res) => {
+  try {
+    const searchRegex = new RegExp(req.query.search, 'i')
+    const filter = req.query.search
+      ? { $or: [{ name: searchRegex }, { description: searchRegex }] }
+      : {}
+    const docs = await Event.find(filter)
+      .lean()
+      .exec()
+
+    res.status(200).json({ data: docs })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export default {
+  ...crudControllers(Event),
+  getMany
+}
--- src/resources/event/event.model.js
+++ src/resources/event/event.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const eventSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    description: { type: String, required: true, trim: true }
+    description: { type: String, required: true, trim: true },
+    tags: [String]
   },
   { timestamps: true }
 )
